c179a0a12668d8ad71807a7a04718c30619c8686
Ray Bramwell <dev@melonbytes.com>
2024-03-11 16:11:04 +0000

    Removed Ansible stuff

--- azurerm_virtual_machine/outputs.tf
+++ azurerm_virtual_machine/outputs.tf
@@ -27,6 +27,7 @@ output "public_ip_address" {
 
 ### The Ansible inventory file
 ### https://www.linkbynet.com/produce-an-ansible-inventory-with-terraform
+/*
 resource "local_file" "AnsibleInventory" {
   content = templatefile("${path.module}/inventory.tmpl",
   {
@@ -50,3 +51,4 @@ resource "local_file" "AnnsibleGroupVar" {
   filename = "${path.module}/../../../inventory/group_vars/${var.environment_nickname}/${var.role}.yml"
   count = var.role == "bastion" || var.require_bastion == false ? 0 : 1
 }
+*/
